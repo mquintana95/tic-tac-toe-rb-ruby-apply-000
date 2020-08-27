b3dd1922486ee4bd474716048cab4563ce3ce00a
Marcus Quintana <mq97325@gmail.com>
2020-08-27 02:37:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
-    if valid_move?(board,index) == true
+    if valid_move?(board,index)
       move(board, index, value = "X")
     else
       turn(board)
