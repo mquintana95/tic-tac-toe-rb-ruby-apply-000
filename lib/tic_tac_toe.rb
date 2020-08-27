1073ae2c3e2930b6191cf40d5d45aa645519c1ee
Marcus Quintana <mq97325@gmail.com>
2020-08-27 02:40:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,7 +115,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    "Congratulations #{winner(board)}!"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
